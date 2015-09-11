c0033b4ec623ffdd98f61ae89a453461e8271b6a
d10xa <d10xa@mail.ru>
2015-09-11 13:22:02 +0300

    hibernate:map-mapping-1 GenericEntity.id -> Long

--- hibernate/map-mapping-1/src/main/java/ru/d10xa/hibernate/GenericEntity.java
+++ hibernate/map-mapping-1/src/main/java/ru/d10xa/hibernate/GenericEntity.java
@@ -3,9 +3,7 @@ package ru.d10xa.hibernate;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
@@ -16,9 +14,6 @@ import java.io.Serializable;
 @EqualsAndHashCode(of = {"id"})
 @ToString(of = {"id"})
 public abstract class GenericEntity implements Serializable {
-   @Id
-   @GeneratedValue(generator = "uuid")
-   @GenericGenerator(name = "uuid", strategy = "uuid2")
-   @Column(length = 36)
-   protected String id;
+   @Id @GeneratedValue
+   protected Long id;
 }
